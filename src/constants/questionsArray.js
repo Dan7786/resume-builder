9f7365b318c2e1b51e8608c5b9f0938b9af20938
Danish <nexxtin.danish77@gmail.com>
2021-09-26 17:17:33 +0530

    initial commit

---
+++ src/constants/questionsArray.js
@@ -0,0 +1,48 @@
+let questionsArray = [
+  {
+    question: "What is your name?",
+    resumeField: "Name",
+    resumeFieldId: "name"
+  },
+  {
+    question: "What is your date of birth?",
+    resumeField: "Dob",
+    resumeFieldId: "dob"
+  },
+  {
+    question: "Email address",
+    resumeField: "Email",
+    resumeFieldId: "email"
+  },
+  {
+    question: "About",
+    resumeField: "About",
+    resumeFieldId: "about"
+  },
+  {
+    question: "Work experience",
+    resumeField: "Work Experience",
+    resumeFieldId: "experience"
+  },
+  {
+    question: "Education",
+    resumeField: "Adress",
+    resumeFieldId: "address"
+  },
+  {
+    question: "What are your skills and abilities",
+    resumeField: "Skills",
+    resumeFieldId: "skills"
+  },
+  {
+    question: "Languages",
+    resumeField: "Language",
+    resumeFieldId: "language"
+  },
+  {
+    question: "Projects",
+    resumeField: "Projects",
+    resumeFieldId: "project"
+  }
+];
+export default questionsArray;
